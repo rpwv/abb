b0170f1188c8a0dccad3aee1062b249f32ce4b6f
ZenityStar <zenitystar@gmail.com>
2022-09-27 12:07:55 +0700

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
 	if (new Date() * 1 - setting.status > 1000) {
 		let _uptime = process.uptime() * 1000
 		let uptime = clockString(_uptime);
-		let bio = `Im ${namebot} 🤖 || ⏰ Aktif Selama ${uptime} || 🌎 Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} || 🎨 Create By ${nameown}`)
+		let bio = `Saya ${namebot} ❗ || ⏰ Aktif Selama ${uptime} || 🕳️ Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} || 📍 Create By ${nameown}`)
 		await conn.updateProfileStatus(bio).catch(_ => _)
 		conn.reply(m.chat, 'Sukses Mengganti Bio Bot', m)
 		setting.status = new Date() * 1
@@ -16,5 +16,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' *Hari ☀️*\n ', h, ' *Jam 🕐*\n ', m, ' *Menit ⏰*\n ', s, ' *Detik ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
 }
